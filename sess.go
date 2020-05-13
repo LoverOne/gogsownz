fabbcd5560a773867dbd1e40d052d66993bee38b
LoverOne <62893196+LoverOne@users.noreply.github.com>
2020-05-13 14:34:33 +0900

    Update sess.go

--- sess.go
+++ sess.go
@@ -5,7 +5,7 @@ import (
   "encoding/gob"
   "io/ioutil"
   "fmt"
-)  
+)
 
 func EncodeGob(obj map[interface{}]interface{}) ([]byte, error) {
   for _, v := range obj {
@@ -20,12 +20,15 @@ func EncodeGob(obj map[interface{}]interface{}) ([]byte, error) {
 func main() {
   var data []byte
   var kv = make(map[interface{}]interface{})
-  kv["uname"]= "administrator"
-  kv["uid"]= int64(1)
-  fmt.Println(kv) 
+  fmt.Print("Input uname:")
+  var uname string
+  fmt.Scanf("%s", &uname)
+  kv["uname"] = uname
+  kv["uid"] = int64(1)
+  fmt.Println(kv)
 
   data, err := EncodeGob(kv);
-  if err !=nil { 
+  if err !=nil {
     fmt.Println(err)
   }
   ioutil.WriteFile("payload", data, 0644)
